5ebee5313add2d1c06470a82bf95960f068379a5
zegermouw <zegermouw@gmail.com>
2023-03-14 10:46:49 +0100

    basic website

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
+
+app.listen(3000);
