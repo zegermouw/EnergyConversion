0704b3596b7d1343071afe5753ee73b4b5c4a867
zegermouw <zegermouw@gmail.com>
2023-03-14 11:54:29 +0100

    basic layout

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
 app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Hey', message: 'Hello there!' })
