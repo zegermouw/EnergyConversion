33e36d5e653dffc26bd372a78d2a62f9cd8ee9af
´D.S.R <d.s.r.doting@umail.leidenuniv.nl>
2023-03-20 22:03:59 +0100

    Fix name of website

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'Energy converter', message: 'Hello there!' })
   })
 
 app.listen(3000);
