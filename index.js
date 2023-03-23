3281101130fbdd425d8961cb5edf408089a2d646
zegermouw <zegermouw@gmail.com>
2023-03-23 09:25:07 +0100

    basic resultpage

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
+const csv=require('csvtojson')
+
+var data = [];
+
+csv()
+.fromFile('./data.csv')
+.then((jsonObj)=>{
+    data = jsonObj
+})
+
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'EnergyConversion', data: data })
   })
+
+app.get('/results', (req, res) => {
+  const testdata = {'number': 200, 'var': 'Watt'}
+
+  res.render('results', {results: testdata})
+})
 
 app.listen(3000);
